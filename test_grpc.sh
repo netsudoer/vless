aea238dbc0b9f01130c61a23ef6150daab2f257c
Peyman Kazemian <61089081+netsudoer@users.noreply.github.com>
2026-02-22 21:27:21 +0330

    Update test_grpc.sh

--- test_grpc.sh
+++ test_grpc.sh
@@ -1,1 +1,7 @@
-curl --http2-prior-knowledge -v http://127.0.0.1:20000/Tun/Tun -H "content-type: application/grpc" --data-binary $'\x00\x00\x00\x00\x00' --max-time 5
+#!/bin/bash
+
+curl --http2-prior-knowledge -v \
+  http://127.0.0.1:20000/Tun/Tun \
+  -H "content-type: application/grpc" \
+  --data-binary $'\x00\x00\x00\x00\x00' \
+  --max-time 5
